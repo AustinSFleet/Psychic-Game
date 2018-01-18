5beef45e813538302db1c3a3c725983392a94e09
AustinSFleet <austinsfleet@gmail.com>
2018-01-18 12:22:57 -0600

    adding mobile version

---
+++ assets/mobileapp.js
@@ -0,0 +1,89 @@
+ 
+var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+
+var randoLet;
+
+
+
+
+
+  
+$(document).ready(function () {
+    
+var guessed = [];
+
+var wins = 0;
+
+var losses = 0;
+
+var lives = 10;
+
+
+
+$(".wins").html(wins);
+
+$(".losses").html(losses);
+
+$(".lives").html(lives);
+
+function newGame () {
+    lives=10;
+    guessed=[];
+    $(".guesses").html(guessed);
+    $(".lives").html(lives);
+    randoLet = alphabet[Math.floor(Math.random() * 26)];
+    console.log(randoLet);
+    
+};
+
+newGame();
+
+$(".newGame").click(function() {
+    newGame();
+});
+
+
+
+
+    //When a key is pressed
+    $("input").keyup(function (letter){
+
+        
+
+        var letterGuess = String.fromCharCode(letter.which).toLowerCase();
+
+        var regex=/^[a-zA-Z]+$/;
+
+        if (!letterGuess.match(regex))
+        {
+            alert("You need to press a letter key!");
+            return false;
+        }
+
+        
+
+        if(guessed.includes(letterGuess) === true) {
+            alert("you've already guessed that letter, pick a new one");
+        } else {
+            guessed.push(letterGuess);
+            $(".guesses").append(letterGuess.toUpperCase());
+        };
+        
+        if (letterGuess === randoLet) {
+            alert("You guessed it! The right letter was " + randoLet.toUpperCase());
+            wins++;
+            $(".wins").html(wins);
+            newGame();
+        } else if (lives > 1) {
+            lives--;
+            $(".lives").html(lives);
+        } else if (lives <= 1){
+            alert("you ran outta lives buckaroo. I was thinking of " + randoLet.toUpperCase() + ". Click OK to try again");
+            losses++;
+            $(".losses").html(losses);
+            newGame();
+        }
+       
+    });
+
+});
